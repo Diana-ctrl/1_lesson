361a2cb15fcdbf76a71dae82eac4dc5fc0a7a48b
Diana <diana.lyah.96@mail.ru>
2021-12-07 11:22:12 +0300

    Updates

---
+++ precache-manifest.c017c199a7dfb9bb6d7724452ac00748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133c893590cb772f58b4343b24d18753",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "0a286587ddf295ea612e",
+    "url": "/ToDoList/static/css/main.7458d352.chunk.css"
+  },
+  {
+    "revision": "74fbe0041453f7a746d8",
+    "url": "/ToDoList/static/js/2.3f979b18.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoList/static/js/2.3f979b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a286587ddf295ea612e",
+    "url": "/ToDoList/static/js/main.c49dcd08.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
