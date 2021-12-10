df0c014a5f676f190f5662b3fd4c081d1683470b
Diana <diana.lyah.96@mail.ru>
2021-12-10 20:56:53 +0300

    add one test for tasks-reducer

--- src/store/todolists-reducer.test.ts
+++ src/store/todolists-reducer.test.ts
@@ -17,8 +17,6 @@ test('correct todolist should be removed', () => {
     expect(endState[0].id).toBe(todolistId2);
 });
 
-
-
 test('correct todolist should be added', () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
