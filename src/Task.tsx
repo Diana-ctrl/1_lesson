562afbf2e6aa6c6c5dcc9ed8f6107a461609668d
Diana <diana.lyah.96@mail.ru>
2021-12-25 16:20:50 +0300

    added destructuring props

--- src/Task.tsx
+++ src/Task.tsx
@@ -14,24 +14,26 @@ type TaskPropsType = {
     removeTask: (taskID: string, toDoListID: string) => void
 }
 
-export const Task = React.memo((props: TaskPropsType) => {
+export const Task = React.memo((props : TaskPropsType) => {
+
+    const {id, title, isDone} = props.task;
 
     const changeTaskTitle = useCallback((title: string) => {
-        props.changeTaskTitle(props.task.id, title, props.todoListID)
-    }, [props.task.id, props.todoListID, props.changeTaskTitle])
+        props.changeTaskTitle(id, title, props.todoListID)
+    }, [id, props.todoListID, props.changeTaskTitle])
 
     return (
-        <div className={props.task.isDone === true ? 'is-done' : ''} key={props.task.id}>
+        <div className={isDone === true ? 'is-done' : ''} key={id}>
             <Checkbox
-                checked={props.task.isDone}
-                onChange={(e) => props.changeTaskStatus(props.task.id, e.currentTarget.checked, props.todoListID)} />
+                checked={isDone}
+                onChange={(e) => props.changeTaskStatus(id, e.currentTarget.checked, props.todoListID)} />
             {/* <input
             type="checkbox"
             onChange={(e) => props.changeTaskStatus(t.id, e.currentTarget.checked, props.id)}
             checked={t.isDone}
             value={t.title}
         /> */}
-            <EditableSpan title={props.task.title} setNewTitle={changeTaskTitle} />
+            <EditableSpan title={title} setNewTitle={changeTaskTitle} />
             <IconButton onClick={() => props.removeTask(props.task.id, props.todoListID)}>
                 <Delete />
             </IconButton>
