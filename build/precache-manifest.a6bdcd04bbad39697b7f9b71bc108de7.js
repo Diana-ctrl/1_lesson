34f48d3666e0167d321e8444f04b47113a6f7ef8
Diana <diana.lyah.96@mail.ru>
2021-12-29 20:31:10 +0300

    add storybook

---
+++ build/precache-manifest.a6bdcd04bbad39697b7f9b71bc108de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cd7b15843087b2e1c465cae4216db6",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "709b0407896562dd3d4d",
+    "url": "/ToDoList/static/css/main.efcc710a.chunk.css"
+  },
+  {
+    "revision": "0941dfb3dca9c396ecae",
+    "url": "/ToDoList/static/js/2.e1aa99f2.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoList/static/js/2.e1aa99f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709b0407896562dd3d4d",
+    "url": "/ToDoList/static/js/main.22507c0b.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
